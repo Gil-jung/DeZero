8fb62b0a495a586601d8c4e58a2a9d2a3339112b
Gil-jung <tornado1227@naver.com>
2024-07-18 18:54:03 +0900

    until step36

--- dezero/__init__.py
+++ dezero/__init__.py
@@ -1,6 +1,6 @@
 # =============================================================================
 # step23.py부터 step32.py까지는 simple_core를 이용해야 합니다.
-is_simple_core = True  # True
+is_simple_core = False  # True
 # =============================================================================
 
 if is_simple_core:
@@ -12,17 +12,17 @@ if is_simple_core:
     from dezero.core_simple import as_variable
     from dezero.core_simple import setup_variable
 
-# else:
-#     from dezero.core import Variable
+else:
+    from dezero.core import Variable
 #     from dezero.core import Parameter
-#     from dezero.core import Function
-#     from dezero.core import using_config
-#     from dezero.core import no_grad
+    from dezero.core import Function
+    from dezero.core import using_config
+    from dezero.core import no_grad
 #     from dezero.core import test_mode
-#     from dezero.core import as_array
-#     from dezero.core import as_variable
-#     from dezero.core import setup_variable
-#     from dezero.core import Config
+    from dezero.core import as_array
+    from dezero.core import as_variable
+    from dezero.core import setup_variable
+    # from dezero.core import Config
 #     from dezero.layers import Layer
 #     from dezero.models import Model
 #     from dezero.datasets import Dataset
