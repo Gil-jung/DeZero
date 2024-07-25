06a1a6032e77f6ea5fbeabddee9a29c940af332d
Gil-jung <tornado1227@naver.com>
2024-07-25 18:55:54 +0900

    until conv2d

--- dezero/functions.py
+++ dezero/functions.py
@@ -168,6 +168,18 @@ def get_item(x, slices):
     return f(x)
 
 
+def expand_dims(x, axis):
+    x = as_variable(x)
+    shape = list(x.shape)
+    shape.insert(axis, 1)
+    return reshape(x, tuple(shape))
+
+
+def flatten(x):
+    """Flattens the input. Does not affect the batch size."""
+    return reshape(x, (x.shape[0], -1))
+
+
 # =============================================================================
 # sum / sum_to / broadcast_to / average / matmul / linear
 # =============================================================================
@@ -492,6 +504,18 @@ def accuracy(y, t):
     return Variable(as_array(acc))
 
 
+def dropout(x, dropout_ratio=0.5):
+    x = as_variable(x)
+
+    if dezero.Config.train:
+        xp = cuda.get_array_module(x)
+        mask = xp.random.rand(*x.shape) > dropout_ratio
+        scale = xp.array(1.0 - dropout_ratio).astype(x.dtype)
+        y = x * mask / scale
+        return y
+    else:
+        return x
+
 
 # =============================================================================
 # max / min / clip
@@ -550,3 +574,23 @@ class Clip(Function):
 
 def clip(x, x_min, x_max):
     return Clip(x_min, x_max)(x)
+
+
+# =============================================================================
+# conv2d / col2im / im2col / basic_math
+# =============================================================================
+from dezero.functions_conv import conv2d
+from dezero.functions_conv import deconv2d
+from dezero.functions_conv import conv2d_simple
+from dezero.functions_conv import im2col
+from dezero.functions_conv import col2im
+# from dezero.functions_conv import pooling_simple
+# from dezero.functions_conv import pooling
+# from dezero.functions_conv import average_pooling
+# from dezero.core import add
+# from dezero.core import sub
+# from dezero.core import rsub
+# from dezero.core import mul
+# from dezero.core import div
+# from dezero.core import neg
+# from dezero.core import pow
